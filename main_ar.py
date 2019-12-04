9ac12ed58701d1acf5e485d1b917bd6962c50830
ledykate <katyakola97@gmail.com>
2019-12-04 15:18:16 +0300

    Работающий архив
    Работающая кнопка для архивирования

--- main_ar.py
+++ main_ar.py
@@ -47,6 +47,18 @@ def codirovanie(str_files): #функция кодирования по коду
         [(chast,n,root)]=heap #определим корень построенного дерева (последний узел)
         root.walk(cod,"") #от корня формируется словарь с кодовыми словами
     return cod #алфавит: символ и его код
+
+def decodirovanie(zakod, alf): #функция декодирования 
+    decod_str=[] #раскодированные символы, но в виде массива
+    bin_ch="" #строка для накопления двоичных символов
+    for b_ch in zakod: #для каждого символа из закодированной строки
+        bin_ch+=b_ch #двоичная строка
+        for keys in alf: #для ключей из алфавита (словаря)
+            if alf.get(keys)==bin_ch: #если значение ключа равно значению двоичной строи
+                decod_str.append(keys) #записываем символ (ключ из словаря) в массив для раскодирования
+                bin_ch="" #обнуляем двоичную строку
+    stroka="".join(decod_str) #формирование строки раскодированной
+    return stroka #декодированная строка
        
 class Transport_main(QMainWindow): #класс, где храняться все действия
     def __init__(self): #служебная функция инициализации,загрузка окна
@@ -54,23 +66,27 @@ class Transport_main(QMainWindow): #класс, где храняться все
          loadUi("design.ui",self) #файл с расположение кнопок и виджета (должны находиться в одной папке)
          
          self.button_arhiv.clicked.connect(self.arhiv) #при нажатии на кнопку "пересчитать" происходит пересчёт
-         #self.button_raspakovka.clicked.connect(self.decodirovanie) #при нажатии на кнопку "решить" происходит поиск решения и считается целевая функция
+         #self.button_raspakovka.clicked.connect(self.raspakovka) #при нажатии на кнопку "решить" происходит поиск решения и считается целевая функция
          #global f
+         self.action_2.triggered.connect(self.showDialog)  
          
-         
+    def showDialog(self): #открытие файла
+        fname = QFileDialog.getOpenFileName(self, 'открыть файл', None, "Текстовый документ (*.txt)")[0]
+        f = open(fname, 'r')
+        with f:
+            data = f.read()
+            self.okno_texta.setText(data)    
     def arhiv(self):
         #global f
         #with open('huff.txt') as files: #открытие файла на чтение
             #f=files.read() #вытаскиваем значения из файла и преобразуем в строку
-         # поле для ввода текста
-        #f = self.okno_texta.toPlainText()
-        #f = self.textEdit.toPlainText()
+         #поле для ввода текста
         f=self.okno_texta.toPlainText()
         f1=self.okno_file.toPlainText()
-        print(f)
-        print(f1)
-        h=".txt"
-        f1=f1+h
+        #print("имя файла")
+        #print(f1)
+        #h=".txt"
+        #f1=f1+h
         cod1 = codirovanie(f) #вызов алгоритма кодирования
         zakod = "".join(cod1[ch] for ch in f) #Сборка строки из списка с разделителем который перед точкой, без этого выведет только словарь
         nyl=0 #объявляем переменную для подсчёта количества нулей в конце, для кратности 8
@@ -89,16 +105,45 @@ class Transport_main(QMainWindow): #класс, где храняться все
             z1[i]=chr(z1[i]) #переводим в символы ascii
         z2="".join(z1) #делаем из массива символов строку
         codec=list(z2.encode('utf-8'))#кодирует строку в байтстроку, используя зарегистрированный кодек (кодировку)
-        with open(f1,"wb") as fout: #байтовая запись в файл
-            fout.write(bytearray(codec))
-        #запись словаря в файл
-        print("")
-        print(codec)
-        with open('slovar.txt','wb') as m_file_cod: #байтовая запись файл словаря
-            pickle.dump(cod1,m_file_cod) #запись специализированного объекта в файл
-        self.reply = QMessageBox.information(self, 'Сообщение',
-            "В таблицах имеются пустные ячейки или где-то есть не число.\nПроверьте ещё раз на заполненость.")
+        if f1=="" or f1==" ":
+            QMessageBox.information(self, 'Предупреждение', "Введите не пустое имя файла!!!!")
+        else:
+            name = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File',f1, "Текстовый документ (*.txt)")
+        #name.setNameFilters(["*.txt"])
+        #name.selectNameFilter("Текстовый документ (*.txt)")
+            with open(name[0],"wb") as fout: #байтовая запись в файл
+                fout.write(bytearray(codec))
+            #запись словаря в файл
+            #print("")
+            #print(codec)
+            with open('slovar.txt','wb') as m_file_cod: #байтовая запись файл словаря
+                pickle.dump(cod1,m_file_cod) #запись специализированного объекта в файл
+            msgbox = QMessageBox(QMessageBox.Information, "Сообщение", "Архивирование прошло успешно. \nИмя вашего файла: %s" % f1, QMessageBox.Ok)
+            msgbox.exec_()
             
+    def raspakovka(self):
+        with open("cod_huff.txt", "rb") as m_file_decod: #открытие файла на байтовое чтение
+            de_codec = m_file_decod.read() #байтовое чтение из файла
+            shufr=list(de_codec.decode('utf-8')) #массив из закодированных символов
+        for i in range(len(shufr)):#для каждого элемента зашифрованного
+            shufr[i]=ord(shufr[i]) #код символа в ASCII
+            shufr[i]=bin(shufr[i])[2:] #перевод этого когда из десятичного числа в двоичное, [2:] нужно, чтобы убрать впереди обозначение 0b
+        for i in range(len(shufr)): #для каждого двоичного числа 
+            while len(shufr[i])%8!=0: #пока разрядность двоичного числа не кратна 8
+                shufr[i]="0"+shufr[i] #Добавляем вперёд ноль
+        if shufr!=[]: #если массив со символами не пустой
+            bit=int(shufr.pop(0),base=2) #первый байт извлекает, переводится в десятичное число и удаляется, этот байт обозначает - сколько нулей в конец добавили
+        else:
+            bit=0 #иначе 0
+        zas="".join(shufr) #закодированная из 0 и 1
+        zas=zas[:(len(zas)-bit)] #удаляем последние добавленные нули в строке
+        #открытие словаря
+        with open('slovar.txt','rb') as m_file_dec: #байтовое чтение файла
+            cod2=pickle.load(m_file_dec) #загружает объект из файла
+        s=decodirovanie(zas, cod2) #декодирование
+        #запись в файл декодированной строки
+        with open("decod_huff.txt", "w") as fi:
+            fi.write(s)
             
 ###-----------------------------------------------------------------------------------------
 #вызов окна 
